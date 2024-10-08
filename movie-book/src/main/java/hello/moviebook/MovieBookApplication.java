730dcd7e25e4e3f889002e5b5cd59c3da0fe4470
KSJ0128 <ksj01128@naver.com>
2024-10-08 23:44:01 +0900

    프로젝트 세팅

---
+++ movie-book/src/main/java/hello/moviebook/MovieBookApplication.java
@@ -0,0 +1,13 @@
+package hello.moviebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieBookApplication.class, args);
+    }
+
+}
---
+++ movie-book/src/test/java/hello/moviebook/MovieBookApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.moviebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
