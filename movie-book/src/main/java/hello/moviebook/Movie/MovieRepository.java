60e223a1e33af9e4209baaf4a9220751351f042e
KSJ0128 <ksj01128@naver.com>
2024-10-09 00:27:05 +0900

    Movie, Book 데이터베이스 구축

---
+++ movie-book/src/main/java/hello/moviebook/Movie/MovieRepository.java
@@ -0,0 +1,8 @@
+package hello.moviebook.Movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
