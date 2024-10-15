b690383e8bfcd332891072315591e21141885f6b
KSJ0128 <ksj01128@naver.com>
2024-10-15 14:46:40 +0900

    장르, 유저 장르 도메인 생성 및 유저 영화 장르 선호도 조사 API 개발

---
+++ movie-book/src/main/java/hello/moviebook/UserGenre/UserGenre.java
@@ -0,0 +1,52 @@
+package hello.moviebook.UserGenre;
+
+import hello.moviebook.User.User;
+import jakarta.persistence.*;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_genre")
+@Setter
+public class UserGenre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_genre_number")
+    private Long userGenreNumber;
+
+    @Column(name = "action", nullable = false)
+    private Boolean action;
+
+    @Column(name = "drama", nullable = false)
+    private Boolean drama;
+
+    @Column(name = "comedy", nullable = false)
+    private Boolean comedy;
+
+    @Column(name = "romance", nullable = false)
+    private Boolean romance;
+
+    @Column(name = "thriller", nullable = false)
+    private Boolean thriller;
+
+    @Column(name = "horror", nullable = false)
+    private Boolean horror;
+
+    @Column(name = "sf", nullable = false)
+    private Boolean sf;
+
+    @Column(name = "fantasy", nullable = false)
+    private Boolean fantasy;
+
+    @Column(name = "animation", nullable = false)
+    private Boolean animation;
+
+    @Column(name = "documentary", nullable = false)
+    private Boolean documentary;
+
+    @Column(name = "crime", nullable = false)
+    private Boolean crime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_number")
+    private User user;
+}
