998a451f7844565c972f9a98395d34f0c1eee175
KSJ0128 <ksj01128@naver.com>
2024-11-03 16:17:28 +0900

    refactor: 유저 영화 장르 관련 Swagger 수정
    UserGenre Swagger Tag, Operation을 수정했습니다.

--- movie-book/src/main/java/hello/moviebook/Movie/controller/UserGenreController.java
+++ movie-book/src/main/java/hello/moviebook/Movie/controller/UserGenreController.java
@@ -1,6 +1,4 @@
 package hello.moviebook.movie.controller;
-
-import hello.moviebook.movie.domain.UserGenre;
 import hello.moviebook.movie.dto.UserGenreReq;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserGenreController {
-    @Operation(summary = "유저 취향 - 영화 장르", description = "유저 영화 장르 선호도 조사 API")
+    @Operation(summary = "유저 선호 장르 설정", description = "유저 영화 선호 장르 설정 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "유저 취향 장르 정보를 저장했습니다.", content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "403", description = "유효성 검사 오류", content = @Content(mediaType = "application/json")),
@@ -19,7 +17,7 @@ public interface UserGenreController {
     })
     public ResponseEntity<String> setPreferredGenre(Authentication principal, @RequestBody UserGenreReq userGenreReq);
 
-    @Operation(summary = "유저 취향 - 영화 장르", description = "유저 영화 장르 비선호도 조사 API")
+    @Operation(summary = "유저 비선호 장르 설정", description = "유저 영화 비선호 장르 설정 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "유저 취향 장르 정보를 저장했습니다.", content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "403", description = "유효성 검사 오류", content = @Content(mediaType = "application/json")),
