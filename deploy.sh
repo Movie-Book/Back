41d22941a62bed3d534535d870e4becdb4c9351e
Seojin <ksj01128@naver.com>
2024-11-18 15:14:34 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,12 @@ PROPERTIES_PATH="/home/mycapisnavy/cicd/application.properties"
 echo "JAR Path: $JAR_PATH"
 
 # application.properties 생성
-echo "$APPLICATION" > $PROPERTIES_PATH
-echo "application.properties created at $PROPERTIES_PATH"
+# echo "$APPLICATION" > $PROPERTIES_PATH
+# echo "application.properties created at $PROPERTIES_PATH"
 
 # JAR 파일에 실행 권한을 부여합니다.
 chmod +x $JAR_PATH
 
 # JAR 파일을 백그라운드에서 실행합니다.
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.config.location=$PROPERTIES_PATH $JAR_PATH > /home/mycapisnavy/cicd/app.log 2>&1 &
 echo "Deployment successful."
