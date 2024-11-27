c8f2e5fc61c41b4114cd45b10a74f3127530f3aa
KSJ0128 <ksj01128@naver.com>
2024-11-27 22:42:48 +0900

    refactor : 친구 리스트 조회 api 응답 수정
    응답으로 FriendListRes를 리턴하는데 친구 id만 리턴해도 충분하기에 name을 삭제했습니다.

--- movie-book/src/main/java/hello/moviebook/friend/dto/FriendListRes.java
+++ movie-book/src/main/java/hello/moviebook/friend/dto/FriendListRes.java
@@ -7,17 +7,14 @@ import lombok.Getter;
 @Getter
 public class FriendListRes {
     private String id;
-    private String name;
 
     @Builder
     public FriendListRes(String userId, Friend friend) {
         if (friend.getUser1().getId().equals(userId)) {
             this.id = friend.getUser2().getId();
-            this.name = friend.getUser2().getId();
         }
         else {
             this.id = friend.getUser1().getId();
-            this.name = friend.getUser1().getId();
         }
     }
 }
