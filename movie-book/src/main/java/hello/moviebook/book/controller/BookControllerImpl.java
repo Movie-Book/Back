4d7aa33a1e2bd0bd2a8315821edb61aa993265d9
KSJ0128 <ksj01128@naver.com>
2024-12-07 17:38:04 +0900

    feat : 도서 추천 API - Controller 구현
    도서 추천 서비스 Controller 구현 및 Swagger를 설정했습니다.

--- movie-book/src/main/java/hello/moviebook/book/controller/BookControllerImpl.java
+++ movie-book/src/main/java/hello/moviebook/book/controller/BookControllerImpl.java
@@ -1,7 +1,9 @@
 package hello.moviebook.book.controller;
 
 import hello.moviebook.book.dto.BookDescriptRes;
+import hello.moviebook.book.dto.RecommendBookRes;
 import hello.moviebook.book.service.BookService;
+import hello.moviebook.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/v1/book")
 @RequiredArgsConstructor
-public class UserBookControllerImpl {
+public class BookControllerImpl {
     private final BookService bookService;
+    private final UserRepository userRepository;
 
     // 줄거리 키워드 기반 도서 검색
     @GetMapping("/search-descript")
@@ -29,5 +32,17 @@ public class UserBookControllerImpl {
     @GetMapping("/search-keyword")
     public ResponseEntity<List<BookDescriptRes>> getKeywordBookList(Authentication authentication) {
         return ResponseEntity.status(HttpStatus.OK).body(bookService.getBookListByMovieKeywords(authentication.getName()));
+    }
+
+    // 사용자 맞춤 추천 도서 리스트
+    @GetMapping("/recommend")
+    public ResponseEntity<List<RecommendBookRes>> getRecommendBookList(Authentication authentication) {
+        if (authentication == null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+
+        List<RecommendBookRes> recBookList = bookService.getRecommendBookList(userRepository.findUserById(authentication.getName()));
+        if (recBookList == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(recBookList);
     }
 }
